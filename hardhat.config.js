4f003bae08855bb19d4528f6378ff8840c58091b
BernabePosadas <bernabe_posadas@yahoo.com>
2022-02-28 13:51:09 +0800

    inignore contract metadata
    Signed-off-by: BernabePosadas <bernabe_posadas@yahoo.com>

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,15 +5,22 @@ require('dotenv').config({path:__dirname + '/.env'});
 const privateKey = process.env.SECRET || "01234567890123456789" 
 const etherscanKey = process.env.API_KEY
 module.exports = {
-  defaultNetwork: "hardhat",
+  defaultNetwork: "mumbai",
   networks: {
     hardhat: {
       chainId: 80001
     },
     mumbai: {
       url: "https://rpc-mumbai.maticvigil.com/",
+      chainId : 80001,
+      accounts: [privateKey]
+    },
+    polygon_main : {
+      url : "https://polygon-rpc.com/",
+      chainId : 137,
       accounts: [privateKey]
     }
+
   },
   solidity: {
     version: "0.8.4",
